4e439df00bca2d87d77dcef70b69e46b21cbc355
Ben Siegel <bendrumin@Bens-MacBook-Pro-2.local>
2017-08-08 16:41:47 -0500

    BS: Fixing a conflict

--- server/models/person.schema.js
+++ server/models/person.schema.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-<<<<<<< HEAD
 // create the Schema
 var personSchema = new Schema({
   name: {type: String, required: true},
@@ -9,24 +8,7 @@ var personSchema = new Schema({
   notes: {type: String, required: true},
   medication: {type: String, required: true},
   dosage: {type: String, required: true}
-=======
-var medicationSchema = new Schema({
-  medication: {type: String},
-  dosage: {type: String}
-});
-
-// create the Schema
-var personSchema = new Schema({
-  name: {type: String, required: true},
-  phoneNumber: {type: String, required: true},
-  notes: {type: String, required: true},
-  allMeds: [] // [medicationSchema]
->>>>>>> server-info
 });
 
 // export our model
 module.exports = mongoose.model('Person', personSchema);
-<<<<<<< HEAD
-=======
-// module.exports = mongoose.model('Medication', medicationSchema);
->>>>>>> server-info
